525111f20eabc5d7d6da4189ebced1266f24565f
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-07-16 02:06:01 -0700

    still working with the code in js script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,65 @@
-var intervalId
-var timer = 10;
-var questionArray = [{
+var questionCounter = 0;
+var ansTimeout = 2000;
 
-    question: 'The Average American does what 22 times a day??',
-    answers: ['Drink Beer', 'Toilet Break', 'Watching TV', 'Opens Fridge'],
-    correctAnswer: 'Opens Fridge',
-}, {
+var correct = 0;
+var incorrect = 0;
+var missed = 0;
 
-    question: 'What is Johny Depp Afraid of?',
-    answers: ['Zuchinni', 'Clown', 'Fart', 'Marriage'],
-    correctAnswer: 'Clown',
-}
-}];
+var questions = [{
 
-$("#startGame").on("click", function() {
-    $("#startGame").replaceWith();
-    startTimer();
-    decrement();
-    firstQuestion();
-    //renderButtons();
-})
+            question: 'The Average American does what 22 times a day?',
+            answers: ['Drink Beer', 'Toilet Break', 'Watching TV', 'Opens Fridge'],
+            correctAnswer: 3
+        },
+        {
 
-function startTimer() {
-    intervalId = setInterval(decrement, 1000);
-}
+            question: 'What is Johny Depp Afraid of?',
+            answers: ['Zuchinni', 'Clown', 'Fart', 'Marriage'],
+            correctAnswer: 1
+        }
 
-function decrement() {
-    timer--;
-    $("#countdown").html("<span>" + timer + "<span>");
+        function submitAnsw() {
+            $("#submit").on("click", function(e) {
+                e.preventDefault();
+                userAns.length = 0;
 
-    if (timer === 0) {
-        stopTimer();
-    }
-}
+                var userSelection = $("#responses input:radio[name=optionsRadios]:checked").val();
+                userAns.push(userSelection);
+                console.log(userAns);
+                nextQ();
+            });
+        };
 
-function stopTimer() {
-    clearInterval(intervalId);
-    nextQuestion();
+        var timeLeft = 8;
+        var increment;
 
-}
+        function runTimer() {
+            increment = setInterval(decrement, 1000);
+        };
+
+        function decrement() {
+            timeLeft--;
+            $("#time-left").html("Time remaining: " + timeLeft + " seconds");
+            if (timeLeft === 0) {
+                stopTimer();
+                userAns.length = 0;
+                //Record user answer to question
+                var userSelection = $("#responses input:radio[name=optionsRadios]:checked").val();
+                userAns.push(userSelection);
+                console.log(userAns);
+                nextQ();
+            };
+        };
+
+        function resetTimer() {
+            timeLeft = 8;
+            $("#time-left").html("Time remaining: " + timeLeft + " seconds");
+        };
+
+        function displayTimer() {
+            $("#time-left").html("Answer Review");
+        };
+
+        function stopTimer() {
+            clearInterval(increment);
+        };
