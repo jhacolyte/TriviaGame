2fd1b339c5b66ba56967bea8437d9646ef22c3e1
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-07-16 00:56:31 -0700

    Completing the index page and start workng on jscript file

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+var intervalId
+var timer = 10;
+var questionArray = [{
+
+    question: 'The Average American does what 22 times a day??',
+    answers: ['Drink Beer', 'Toilet Break', 'Watching TV', 'Opens Fridge'],
+    correctAnswer: 'Opens Fridge',
+}, {
+
+    question: 'What is Johny Depp Afraid of?',
+    answers: ['Zuchinni', 'Clown', 'Fart', 'Marriage'],
+    correctAnswer: 'Clown',
+}
+}];
