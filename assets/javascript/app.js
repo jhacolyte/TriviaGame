a238015bfd09e7d997d22e166bea058df6b2c921
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-07-18 13:20:50 -0700

    recoded everything to make the apps working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,136 @@
-var questionCounter = 0;
-var ansTimeout = 2000;
+$(document).ready(function() {
 
-var correct = 0;
-var incorrect = 0;
-var missed = 0;
 
-var questions = [{
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click', '.option', trivia.guessChecker);
 
-            question: 'The Average American does what 22 times a day?',
-            answers: ['Drink Beer', 'Toilet Break', 'Watching TV', 'Opens Fridge'],
-            correctAnswer: 3
-        },
-        {
+})
 
-            question: 'What is Johny Depp Afraid of?',
-            answers: ['Zuchinni', 'Clown', 'Fart', 'Marriage'],
-            correctAnswer: 1
+var trivia = {
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId: '',
+
+    questions: {
+        q1: 'What is Johnny Depp afraid of?',
+        q2: 'Washington police officers get a half hour class in how to what?',
+
+    },
+    options: {
+        q1: ['Marriage', 'Cockroach', 'Burrito', 'Clowns'],
+        q2: ['Talking', 'Laughing', 'Potty Breaks', 'Sit Down'],
+
+    },
+    answers: {
+        q1: 'Clowns',
+        q2: 'Sit Down',
+    },
+
+    startGame: function() {
+        trivia.currentSet = 0;
+        trivia.correct = 0;
+        trivia.incorrect = 0;
+        trivia.unanswered = 0;
+        clearInterval(trivia.timerId);
+
+        $('#game').show();
+
+        $('#results').html('');
+
+        $('#timer').text(trivia.timer);
+
+        $('#start').hide();
+
+        $('#remaining-time').show();
+
+        trivia.nextQuestion();
+
+    },
+    nextQuestion: function() {
+
+        trivia.timer = 20;
+        $('#timer').removeClass('last-seconds');
+        $('#timer').text(trivia.timer);
+
+        if (!trivia.timerOn) {
+            trivia.timerId = setInterval(trivia.timerRunning, 1000);
         }
 
-        function submitAnsw() {
-            $("#submit").on("click", function(e) {
-                e.preventDefault();
-                userAns.length = 0;
+        var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+        $('#question').text(questionContent);
 
-                var userSelection = $("#responses input:radio[name=optionsRadios]:checked").val();
-                userAns.push(userSelection);
-                console.log(userAns);
-                nextQ();
-            });
-        };
+        var questionOptions = Object.values(trivia.options)[trivia.currentSet];
 
-        var timeLeft = 8;
-        var increment;
+        $.each(questionOptions, function(index, key) {
+            $('#options').append($('<button class="option btn btn-info btn-lg">' + key + '</button>'));
+        })
 
-        function runTimer() {
-            increment = setInterval(decrement, 1000);
-        };
+    },
+    timerRunning: function() {
+        if (trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length) {
+            $('#timer').text(trivia.timer);
+            trivia.timer--;
+            if (trivia.timer === 4) {
+                $('#timer').addClass('last-seconds');
+            }
+        } else if (trivia.timer === -1) {
+            trivia.unanswered++;
+            trivia.result = false;
+            clearInterval(trivia.timerId);
+            resultId = setTimeout(trivia.guessResult, 1000);
+            $('#results').html('<h3>Out of time! The answer was ' + Object.values(trivia.answers)[trivia.currentSet] + '</h3>');
+        } else if (trivia.currentSet === Object.keys(trivia.questions).length) {
 
-        function decrement() {
-            timeLeft--;
-            $("#time-left").html("Time remaining: " + timeLeft + " seconds");
-            if (timeLeft === 0) {
-                stopTimer();
-                userAns.length = 0;
-                //Record user answer to question
-                var userSelection = $("#responses input:radio[name=optionsRadios]:checked").val();
-                userAns.push(userSelection);
-                console.log(userAns);
-                nextQ();
-            };
-        };
+            $('#results')
+                .html('<h3>Thank you for playing!</h3>' +
+                    '<p>Correct: ' + trivia.correct + '</p>' +
+                    '<p>Incorrect: ' + trivia.incorrect + '</p>' +
+                    '<p>Unaswered: ' + trivia.unanswered + '</p>' +
+                    '<p>Please play again!</p>');
 
-        function resetTimer() {
-            timeLeft = 8;
-            $("#time-left").html("Time remaining: " + timeLeft + " seconds");
-        };
+            $('#game').hide();
 
-        function displayTimer() {
-            $("#time-left").html("Answer Review");
-        };
+            $('#start').show();
+        }
 
-        function stopTimer() {
-            clearInterval(increment);
-        };
+    },
+    guessChecker: function() {
+
+        // var resultId;
+
+        var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+
+        if ($(this).text() === currentAnswer) {
+            $(this).addClass('btn-success').removeClass('btn-info');
+
+            trivia.correct++;
+            clearInterval(trivia.timerId);
+            resultId = setTimeout(trivia.guessResult, 1000);
+            $('#results').html('<h3>Correct Answer!</h3>');
+        } else {
+            $(this).addClass('btn-danger').removeClass('btn-info');
+
+            trivia.incorrect++;
+            clearInterval(trivia.timerId);
+            resultId = setTimeout(trivia.guessResult, 1000);
+            $('#results').html('<h3>Better luck next time! ' + currentAnswer + '</h3>');
+        }
+
+    },
+    guessResult: function() {
+
+        trivia.currentSet++;
+
+        $('.option').remove();
+        $('#results h3').remove();
+
+        trivia.nextQuestion();
+
+    }
+
+}
